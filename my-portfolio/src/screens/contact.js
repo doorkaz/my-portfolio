58231c0024bf0a6b9b9ff864f6b7c00de368bcee
doorkaz <retamalespj@gmail.com>
2023-12-16 19:06:18 -0300

    Copy to clipboard Contact

--- my-portfolio/src/screens/contact.js
+++ my-portfolio/src/screens/contact.js
@@ -1,37 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../components/styles.module.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+
 const Contact = () => {
+    const [copied, setCopied] = useState(null);
+
+    function handleCopyClick(event){
+        const id = event.target.id;
+        navigator.clipboard.writeText(event.target.innerText);
+        setCopied(id);
+    };
     return (
         <>
             <footer id="contact" className={styles.footer}>
                 <Row>
-                    <Col xs={2} md={2} lg={2}>
+                    <Col xs={4} md={4} lg={4}>
                         <h3>Contáctame</h3>
                     </Col>
-                    <Col xs={5} md={5} lg={5}>
+                    <Col xs={4} md={4} lg={4} className="d-flex flex-column align-items-start justify-content-start">
                         <Row>
-                            <div className='d-flex me-4'>
-                                <div className='me-4'>
-                                    <i className='bi bi-linkedin'/> 
-                                    <a className={styles["link"]} href="https://www.linkedin.com/in/pablo-retamales-jara-b95511232/"> linkedin.com/in/pablo-retamales-jara-b95511232</a>
-                                </div>
-                                <div className='me-4'>
-                                    <i className='bi bi-telephone'/>
-                                    <span> +56 9 8947 6136</span> 
-                                </div>
+                            <div>
+                                <i className='bi bi-linkedin'/> 
+                                <a className={styles["link"]} href="https://www.linkedin.com/in/pablo-retamales-jara-b95511232/"> linkedin.com/in/pablo-retamales-jara-b95511232</a>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className='d-flex align-items-start justify-content-start'>
+                                <i className='bi bi-telephone'/>
+                                <a
+                                id="phone"
+                                className={styles["link"]} 
+                                href="/#" 
+                                onClick={handleCopyClick}
+                                > 
+                                    +56 9 8947 6136
+                                </a> 
+                                <img src="/images/contact/copytoclipboard.png" width='22' alt="c"/>
+                                {copied ==="phone" && <p>(Copiado)</p>}
                             </div>
                         </Row>
                     </Col>
-                    <Col xs={5} md={5} lg={5}>
+                    <Col xs={4} md={4} lg={4} className="d-flex flex-column align-items-start justify-content-start">
                         <Row>
-                            <div className='d-flex me-4'>
-                                <div>
-                                    <i class="bi bi-github"></i>
-                                    <a className={styles["link"]} href="https://github.com/doorkaz"> github.com/doorkaz</a>
-                                </div>
-                            </div> 
+                            <div>
+                                <i class="bi bi-github"></i>
+                                <a className={styles["link"]} href="https://github.com/doorkaz"> github.com/doorkaz</a>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className='d-flex align-items-start justify-content-start'>
+                                <i className="bi bi-envelope"></i>
+                                <a
+                                id="email"
+                                className={styles["link"]} 
+                                href="/#" 
+                                onClick={handleCopyClick}
+                                > 
+                                retamalespj@gmail.com
+                                </a> 
+                                <img src="/images/contact/copytoclipboard.png" width='22' alt="c"/>
+                                {copied ==="email" && <p>(Copiado)</p>}
+                            </div>
                         </Row>
                     </Col>
                 </Row>
