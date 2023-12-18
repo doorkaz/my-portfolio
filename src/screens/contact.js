d2625a5591c3d2227db2fd09b95408864fc0a7f6
doorkaz <retamalespj@gmail.com>
2023-12-18 15:51:02 -0300

    styles modified

--- src/screens/contact.js
+++ src/screens/contact.js
@@ -13,19 +13,19 @@ const Contact = () => {
     };
     return (
         <>
-            <footer id="contact" className={`px-4 ${styles.footer}`}>
+            <footer id="contact" className={styles["footer"]}>
                 <Row className="d-flex align-items-center justify-content-center px-4">
                     <Col xs={12} sm={12} md={12} lg={2} className="d-flex flex-column align-items-center justify-content-center">
                         <h3>Contáctame</h3>
                     </Col>
-                    <Col xs={12} sm={12} md={7} lg={5} className={`d-flex flex-column ${styles["contact-responsive"]}`}>
-                        <div className='d-inline align-items-center justify-content-center'>
+                    <Col xs={12} sm={12} md={7} lg={5} className={styles["contact-column-res"]}>
+                        <div className={styles["footer-item"]}>
                             <i className='bi bi-linkedin me-1'/>
                             <a className={styles["link"]} href="https://www.linkedin.com/in/pablo-retamales-jara-b95511232/"> 
                             linkedin.com/in/pablo-retamales-jara-b95511232
                             </a>
                         </div>
-                        <div className='d-inline align-items-center justify-content-center'>
+                        <div className={styles["footer-item"]}>
                             <i className='bi bi-telephone me-1' />
                             <a id="phone" className={styles["link"]} href="#/" onClick={handleCopyClick}> 
                                 +56 9 8947 6136
@@ -34,12 +34,12 @@ const Contact = () => {
                             {copied ==="phone" && <p className={`${styles["copy-to-clipboard"]}`} style={{ display: "inline" }}>(Copiado)</p>}
                         </div>
                     </Col>
-                    <Col xs={12} sm={12} md={5} lg={5} className={`d-flex flex-column ${styles["contact-responsive"]}`}>
-                        <div className='d-inline align-items-center justify-content-center'>
+                    <Col xs={12} sm={12} md={5} lg={5} className={styles["contact-column-res"]}>
+                        <div className={styles["footer-item"]}>
                             <i class="bi bi-github me-1"/>
                             <a className={styles["link"]} href="https://github.com/doorkaz"> github.com/doorkaz</a>
                         </div>
-                        <div className='d-inline align-items-center justify-content-center'>
+                        <div className={styles["footer-item"]}>
                             <i className="bi bi-envelope me-1"/>
                             <a id="email" className={styles["link"]} href="#/" onClick={handleCopyClick}> 
                             retamalespj@gmail.com
