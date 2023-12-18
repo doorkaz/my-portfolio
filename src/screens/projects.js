44d00ddda176c73f2e9b76313eb96e5862ded754
doorkaz <retamalespj@gmail.com>
2023-12-18 17:02:39 -0300

    title set

--- src/screens/projects.js
+++ src/screens/projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
     return (
         <>
-            <section id="projects">
+            <section id="projects" className='mt-2'>
                 <h3>Mis proyectos</h3>
             </section>
         </>
