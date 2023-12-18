cb101629e5659122e1d8e775df95991d44591a8d
doorkaz <retamalespj@gmail.com>
2023-12-18 03:00:47 -0300

    yup, indicators={false}

--- src/screens/about.js
+++ src/screens/about.js
@@ -11,27 +11,27 @@ const about = () => {
                 <p>Profesional en formación, autodidacta y altamente motivado, con gran capacidad de resolución y adaptación. Dispuesto a aprender y mejorar en el ámbito social y laboral. Actualmente, estoy cursando Ingeniería Civil Informática en la Universidad Técnica Federico Santa María, donde recién concluí mi tercer año de carrera. </p>      
                 <h3>Mis principales competencias</h3>
                 <Row className='d-flex flex-row align-items-center justify-content-around'>
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>PYTHON</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/python.png"} alt="python"/>
                     </Col> 
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>REACTJS</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/reactjs.png"} alt="reactjs"/>
                     </Col> 
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>JAVASCRIPT</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/javascript.png"} alt="javascript"/>
                     </Col> 
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>SQL</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/sql.png"} alt="sql"/>
                     </Col> 
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>CSS</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/css.svg"} alt="css"/>
                     </Col> 
-                    <Col xs={4} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span>BOOTSTRAP</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/bootstrap.png"} alt="bootstrap"/>
                     </Col> 
--- src/screens/carousel-projects.js
+++ src/screens/carousel-projects.js
@@ -1,25 +1,26 @@
 import Carousel from 'react-bootstrap/Carousel';
 import styles from '../components/styles.module.css';
+
 function CarouselProjects() {
   return (
 	<div className="d-flex flex-column align-items-center justify-content-center mx-5">
-		<Carousel fade className={styles["carousel-responsive"]}>
+		<Carousel indicators={false} className={styles["carousel-responsive"]}>
 			<Carousel.Item interval={2000} className={`${styles["carousel-item-responsive"]}`}>
 				<img className={styles["carousel-img"]} src={process.env.PUBLIC_URL +"/images/carousel/claps.png"} alt="First slide"/>
 				<Carousel.Caption className={styles["glass-efect"]}>
-					<h3 className={styles["carousel-caption-text"]}>Claps App</h3>	
+					<h3>Claps App</h3>	
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item interval={2000} className={`${styles["carousel-item-responsive"]}`}>
 				<img className={styles["carousel-img"]} src={process.env.PUBLIC_URL +"/images/carousel/easytake.png"} alt="Second slide"/>
 				<Carousel.Caption className={styles["glass-efect"]}>
-					<h3 className={styles["carousel-caption-text"]}>EasyTake</h3>
+					<h3>EasyTake</h3>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item interval={2000} className={`${styles["carousel-item-responsive"]}`}>
 				<img className={styles["carousel-img"]} src={process.env.PUBLIC_URL +"/images/carousel/prestigetravels.png"} alt="Third slide"/>
 				<Carousel.Caption className={styles["glass-efect"]}>
-					<h3 className={styles["carousel-caption-text"]}>PrestigeTravels</h3>
+					<h3>PrestigeTravels</h3>
 				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
