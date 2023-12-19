a34bce6afad48409eed0de34888e7ea29c54b290
doorkaz <retamalespj@gmail.com>
2023-12-19 00:59:02 -0300

    navbar dev icon

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,18 +6,23 @@ import styles from './styles.module.css';
 
 const CustomNavbar = () => {
     return (
-        <Navbar id="navbar" expand="lg" className={`bg-body-terciary bg-white shadow-sm p-3 rounded ${styles["navbar-fix"]}`}>
+        <Navbar id="navbar" expand="lg" className={`bg-body-terciary shadow-lg p-3 ${styles["navbar-fix"]}`}>
             <Container>
-                <Navbar.Brand className="d-flex flex-column justify-content-start align-items-start fs-6 ms-2" style={{ lineHeight: '1' }}>
-                    <span>Pablo</span>
-                    <span>Retamales</span>
+                <Navbar.Brand style={{ color: 'white' }}>
+                    <div className={styles["dev-icon"]}>
+                        &lt;/&gt;
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Brand className="d-flex flex-column justify-content-start align-items-start ms-2" style={{ lineHeight: '1', color: 'white' }}>
+                    <span className='fs-5'>Pablo</span>
+                    <span className='fs-5'>Retamales</span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link href="#about">Sobre mí</Nav.Link>
-                    <Nav.Link href="#projects">Proyectos</Nav.Link>
-                    <Nav.Link href="#contact">Contacto</Nav.Link>
+                    <Nav.Link style={{color: 'white'}} href="#about">Sobre mí</Nav.Link>
+                    <Nav.Link style={{color: 'white'}} href="#projects">Proyectos</Nav.Link>
+                    <Nav.Link style={{color: 'white'}} href="#contact">Contacto</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
