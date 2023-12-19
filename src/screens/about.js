b601ae851efbbbf954db48a6d0b7fc42e982e280
doorkaz <retamalespj@gmail.com>
2023-12-19 02:29:08 -0300

    skills 2 in a row for mobile

--- src/screens/about.js
+++ src/screens/about.js
@@ -11,27 +11,27 @@ const about = () => {
                 <p>Profesional en formación, autodidacta y altamente motivado, con gran capacidad de resolución y adaptación. Dispuesto a aprender y mejorar en el ámbito social y laboral. Actualmente, estoy cursando Ingeniería Civil Informática en la Universidad Técnica Federico Santa María, donde recién concluí mi tercer año de carrera. </p>      
                 <h3 className='fs-4'>Mis principales competencias</h3>
                 <Row className='d-flex flex-row align-items-center justify-content-around'>
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>Python</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/python.png"} alt="python"/>
                     </Col> 
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>ReactJS</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/reactjs.png"} alt="reactjs"/>
                     </Col> 
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>JavaScript</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/javascript.png"} alt="javascript"/>
                     </Col> 
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>SQL</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/sql.png"} alt="sql"/>
                     </Col> 
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>CSS</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/css.svg"} alt="css"/>
                     </Col> 
-                    <Col xs={4} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                    <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                         <span className={`fs-6 ${styles["skills-text"]}`}>Bootstrap</span>
                         <img className={styles["skills-img"]} src={process.env.PUBLIC_URL +"/images/skills/bootstrap.png"} alt="bootstrap"/>
                     </Col> 
