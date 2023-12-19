2a4546eac2ab0428febe65dade9a9e9552ef05f2
doorkaz <retamalespj@gmail.com>
2023-12-19 03:18:15 -0300

    scroll margin

--- src/screens/about.js
+++ src/screens/about.js
@@ -8,7 +8,7 @@ const about = () => {
         <>
             <section id="about" className={`mt-5 ${styles.aboutme}`}>
                 <h1 className='fs-2'>Hola. Soy Pablo <img className={styles["hi"]} src={process.env.PUBLIC_URL +"/images/hi.png"} alt="hi"/></h1>
-                <p>Profesional en formación, autodidacta y altamente motivado, con gran capacidad de resolución y adaptación. Dispuesto a aprender y mejorar en el ámbito social y laboral. Actualmente, estoy cursando Ingeniería Civil Informática en la Universidad Técnica Federico Santa María, donde recién concluí mi tercer año de carrera. </p>      
+                <p>Profesional en formación, autodidacta y altamente motivado, con gran capacidad de resolución y adaptación. Dispuesto a aprender y mejorar en el ámbito social y laboral. Actualmente, estoy cursando Ingeniería Civil Informática en la Universidad Técnica Federico Santa María, donde recién concluí mi tercer año de carrera.</p>      
                 <h3 className='fs-4'>Mis principales competencias</h3>
                 <Row className='d-flex flex-row align-items-center justify-content-around'>
                     <Col xs={6} sm={4} md={4} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
--- tasks.md
+++ tasks.md
@@ -2,4 +2,7 @@
 
 * Agregar stack de tecnologías a cada proyecto.
 * Describir los proyectos.
-* Idealmente arreglar que los links a los id se redirija perfecto. 
+* Añadir soft skills
+* Añadir intereses
+* Añadir idiomas
+* Idealmente arreglar que los links te lleven a los id de forma perfecta. (Creo que no es posible)
