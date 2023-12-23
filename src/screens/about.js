1e7481c9522a535298cb9db28e270e6e08be4032
doorkaz <retamalespj@gmail.com>
2023-12-23 20:56:21 -0300

    fix deleted img display none

--- src/screens/about.js
+++ src/screens/about.js
@@ -5,7 +5,7 @@ const about = () => {
     return (
         <>
             <section id="about" className={`mt-5 ${styles.aboutme}`}>
-                <h1 className='fs-2'>Hola. Soy Pablo! <img className={styles["hi"]} style={{display: 'none'}} src={process.env.PUBLIC_URL +"/images/hi.png"} alt="hi"/></h1>
+                <h1 className='fs-2'>Hola. Soy Pablo!</h1>
                 <p>Soy responsable, riguroso, altamente motivado, con gran capacidad de resolución y adaptabilidad. Las áreas de mi interés son el desarrollo full-stack y el análisis de datos. Me gusta el código limpio y eficiente, ah!, y los gatos!</p>
             </section>
         </>
