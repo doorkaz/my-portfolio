81e4ee4335944c062852077d79f18667a29855a7
doorkaz <retamalespj@gmail.com>
2023-12-23 20:45:50 -0300

    estudios responsive

--- src/screens/education.js
+++ src/screens/education.js
@@ -7,15 +7,15 @@ const Education = () => {
         <>
             <section id="education">
                 <h3 className='fs-4'>Mis estudios</h3>
-                <div className='d-flex align-items-center justify-content-center'>
+                <div className='d-flex align-items-center justify-content-center mx-5'>
                     <Row>
-                        <Col  className='text-start'>
+                        <Col xs={12} sm={6} className='text-start'>
                             <h3 className='fs-4'>Formación académica</h3>
                             <p className="fs-5" style={{margin: 0}}>Ingeniería Civil Informática</p>
                             <p className="fs-6 fst-italic lh-sm">Universidad Técnica Federico Santa María (UTFSM)<br/>2021 - Actualidad</p>
                         </Col>
-                        <Col className='text-start'>
-                            <h3 className='flex fs-4 mb-auto'>Formación adicional</h3>
+                        <Col xs={12} sm={6} className='text-start'>
+                            <h3 className='fs-4'>Formación adicional</h3>
                             <p className='fs-5' style={{margin: 0}}>Python sin fronteras: HTML, CSS, Flask y MySQL. </p>
                             <p className='fs-6 fst-italic lh-sm'>Plataforma Udemy</p>
                         </Col>
