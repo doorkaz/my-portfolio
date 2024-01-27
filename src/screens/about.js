aac901cd6163fbd5890aa0d1d98e16aeb78b7193
doorkaz <retamalespj@gmail.com>
2024-01-27 15:28:36 -0300

    Skills

--- src/screens/about.js
+++ src/screens/about.js
@@ -7,7 +7,7 @@ const about = () => {
             <section id="about" className={`mt-5 ${styles.aboutme}`}>
                 <h1 className='fs-2'>Hola. Soy Pablo!</h1>
                 <p>Soy motivado, responsable y riguroso. Me gusta resolver problemas complejos. Tengo conocimientos en varios lenguajes de programación, bases de datos, desarrollo de software, análisis y desarrollo de algoritmos.</p>
-                <h3 class="fs-4">Áreas de interés</h3>
+                <h3 className="fs-4">Áreas de interés</h3>
                 <p>Las áreas de mi interés son el desarrollo full-stack y análisis de datos.</p>
             </section>
         </>
--- src/screens/skills.js
+++ src/screens/skills.js
@@ -7,44 +7,82 @@ import Carousel from 'react-bootstrap/Carousel';
 const skills = () => {
     return (
         <>
-            <section id="skills" className={`mt-4 ${styles.aboutme}`}>
+            <section id="skills" className={`d-flex flex-column justify-content-center text-center`} style={{margin: "0 6em 0 6em"}}>
                 <h3 className='fs-4'>Mi stack de tecnologías</h3>
                 <Carousel className={styles["carousel-min-height"]} interval={null} data-bs-theme="dark" controls={false}>
                     <Carousel.Item>
                         {/* Primer stack de tecnologías */}
                         <Row className='d-flex align-items-center justify-content-around'>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
-                                <span className="fs-6">Python</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' alt="1"/>
-                            </Col> 
-                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
-                                <span className="fs-6">ReactJS</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' alt="2"/>
-                            </Col> 
+                                <span className="fs-6">React</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' alt="img"/>
+                            </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">JavaScript</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' alt="3"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' alt="img"/>
                             </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">TypeScript</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' alt="3"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">Django</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg' alt="img"/>
+                            </Col> 
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">Python</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' alt="img"/>
                             </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">Java</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' alt="5"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' alt="img"/>
                             </Col>  
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">C</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' alt="2"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' alt="img"/>
                             </Col> 
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">C++</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg' alt="3"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg' alt="img"/>
                             </Col> 
+                        </Row>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Row className='d-flex align-items-center justify-content-around'>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">NumPy</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original-wordmark.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">Matplotlib</span>
+                                <img className={styles["skills-img"]} src='https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg' alt="img"/>
+                            </Col>
+                           
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">Pandas</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">Excel</span>
+                                <img className={styles["skills-img"]} src='https://upload.wikimedia.org/wikipedia/commons/7/73/Microsoft_Excel_2013-2019_logo.svg' alt="img"/>
+                            </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">PHP</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg' alt="3"/>
-                            </Col> 
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">MySQL</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">MongoDB</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">SQL Server</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain.svg' alt="img"/>
+                            </Col>
+                            
                         </Row>
                     </Carousel.Item>
                     
@@ -52,36 +90,38 @@ const skills = () => {
                     <Carousel.Item>
                         <Row className='d-flex align-items-center justify-content-around'>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
-                                <span className="fs-6">MySQL</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg' alt="4"/>
-                            </Col>
-                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
-                                <span className="fs-6">MongoDB</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' alt="4"/>
-                            </Col>
-                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
-                                <span className="fs-6">CSS</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' alt="5"/>
+                                <span className="fs-6">CSS3</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' alt="img"/>
                             </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">HTML5</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' alt="5"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' alt="img"/>
                             </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">Bootstrap</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' alt="6"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' alt="img"/>
                             </Col> 
+                            
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">LaTeX</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/latex/latex-original.svg' alt="img"/>
+                            </Col>
+                            
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">Android Studio</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg' alt="1"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg' alt="img"/>
                             </Col> 
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">Jupyter Notebook</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg' alt="4"/>
-                            </Col> 
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg' alt="img"/>
+                            </Col>
+                            <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
+                                <span className="fs-6">VSCode</span>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg' alt="img"/>
+                            </Col>
                             <Col xs={6} sm={3} md={3} lg={1} className='d-flex flex-column align-items-center justify-content-center text-center'>
                                 <span className="fs-6">GitHub</span>
-                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="3"/>
+                                <img className={styles["skills-img"]} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="img"/>
                             </Col> 
                         </Row>
                     </Carousel.Item>
