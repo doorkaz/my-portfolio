325249086a692d067e284426bcef1c4013ee2e70
doorkaz <retamalespj@gmail.com>
2024-12-28 18:41:04 -0300

    feat: add reusable buttonLink

---
+++ components/ButtonLink.tsx
@@ -0,0 +1,19 @@
+import React, { AnchorHTMLAttributes, JSX } from 'react'
+import { IconType } from 'react-icons'
+
+interface ButtonLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  icon?: IconType
+}
+
+export const ButtonLink = (props: ButtonLinkProps): JSX.Element => {
+  const { icon: Icon, className, children, ...rest } = props
+  return (
+    <a
+      className={`inline-flex justify-center items-center border border-neutral-400 rounded-2xl px-6 py-3 ${className ?? ''}`}
+      {...rest}
+    >
+      {(Icon != null) && <Icon className='me-2' />}
+      <span className='text-xl'>{children}</span>
+    </a>
+  )
+}
