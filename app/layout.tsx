36cb9f28d736769b1c6e5ae162fe1fc4f58c5e96
doorkaz <retamalespj@gmail.com>
2024-12-28 16:22:26 -0300

    chore: clean up initial project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { JSX } from 'react'
 import type { Metadata } from 'next'
-import './globals.css'
+import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Retamales Pablo',
