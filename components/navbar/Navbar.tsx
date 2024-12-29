e97625d34bc1c6d88e5252dd2c3f28f4d17b079a
doorkaz <retamalespj@gmail.com>
2024-12-29 16:16:13 -0300

    fix: fixed accessibility issues

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,15 +7,17 @@ import clsx from 'clsx'
 export const Navbar = (): JSX.Element => {
   const { isScrollingUp, isScrolledPastScreen } = useScroll()
 
-  const navbarStyles = 'list-none m-0 flex flex-row px-12 py-8 top-0 left-0 w-full fixed bg-neutral-900 transition-transform duration-300 z-50'
+  const navbarStyles = 'fixed flex flex-row px-12 py-8 w-full transition-transform duration-300 z-50'
   const scrollStyles = isScrollingUp ? 'translate-y-0' : '-translate-y-full'
   const scrollPastHeroSection = isScrolledPastScreen ? 'bg-neutral-900 shadow backdrop-blur-[3px]' : 'bg-transparent'
 
   return (
     <nav className={clsx(navbarStyles, scrollStyles, scrollPastHeroSection)}>
-      <NavLink href='#'>Inicio</NavLink>
-      <NavLink href='#projects'>Proyectos</NavLink>
-      <NavLink href='#contact'>Contacto</NavLink>
+      <ul className='flex list-none m-0'>
+        <NavLink href='#'>Inicio</NavLink>
+        <NavLink href='#projects'>Proyectos</NavLink>
+        <NavLink href='#contact'>Contacto</NavLink>
+      </ul>
     </nav>
   )
 }
