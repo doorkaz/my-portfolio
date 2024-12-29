410311574100f8e8b8fbfb275691acf31a913225
doorkaz <retamalespj@gmail.com>
2024-12-29 16:21:47 -0300

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ import { Skills } from './components/skills/Skills'
 import { Projects } from './components/projects/Projects'
 import { FAQ as FrequentlyAskedQuestions } from './components/faq/FAQ'
 import { Footer } from './components/Footer'
-import { Navbar } from '@/components/navbar/Navbar'
+import { Header } from '@/components/Header'
 
 export default function Home (): JSX.Element {
   return (
     <>
-      <Navbar />
+      <Header />
+
       <HeroSection />
       <Container className='flex flex-col py-8 gap-8'>
         <AboutMe />
@@ -19,6 +20,7 @@ export default function Home (): JSX.Element {
         <Projects />
         <FrequentlyAskedQuestions />
       </Container>
+
       <Footer />
     </>
   )
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react'
+import { Navbar } from './navbar/Navbar'
+
+export const Header = (): JSX.Element => {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  )
+}
