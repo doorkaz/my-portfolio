dcd5ac81e59af0e4bbb16504472518ecf5902e18
doorkaz <retamalespj@gmail.com>
2024-12-29 00:28:42 -0300

    fix(projectCard): fixed mobile styles

--- app/components/projects/ProjectCard.tsx
+++ app/components/projects/ProjectCard.tsx
@@ -8,9 +8,9 @@ export const ProjectCard = (project: Project): JSX.Element => {
   const imageSrc = (project.images.length > 0) ? project.images[0] : DefaultCardImage
 
   return (
-    <article className='flex flex-col xl:flex-row xl:items-start bg-neutral-800 w-full h-full hyphens-auto xl:hyphens-none xs:h-[500px] md:h-[540px] xl:h-[480px] rounded-3xl px-8 pt-4 overflow-hidden' lang='de'>
+    <article className='flex flex-col xl:flex-row xl:items-start bg-neutral-800 w-full h-full hyphens-auto xl:hyphens-none xl:h-[550px] 2xl:h-[600px] rounded-3xl px-8 pt-4 overflow-hidden' lang='de'>
       {/* Descripción */}
-      <div className='text-start w-full h-1/2 xl:h-full xl:w-3/10 xl:me-8'>
+      <div className='text-start w-full h-full xl:w-5/12 xl:me-8'>
         <small className='text-base'>{project.type} - {project.job} - {project.year}</small>
         <H2 className='font-bold text-neutral-300 my-3'>{project.name}</H2>
         <P className=' font-lighter'>{project.description}</P>
@@ -20,7 +20,7 @@ export const ProjectCard = (project: Project): JSX.Element => {
       </div>
 
       {/* Imagen */}
-      <figure className='aspect-[4/3] w-full xl:w-7/10 h-full block relative rounded-t-3xl overflow-hidden'>
+      <figure className='aspect-[4/3] w-full xl:w-7/12 h-full block relative rounded-t-2xl xl:rounded-t-3xl overflow-hidden'>
         <Image
           className='w-auto h-full object-cover object-center'
           src={imageSrc}
