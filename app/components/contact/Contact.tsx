c6505b44b09f20ddf76a953174acbc4e0f7e7b0a
doorkaz <retamalespj@gmail.com>
2024-12-29 23:51:33 -0300

    fix: Fix contact styles on extra small devices <=400px

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -6,10 +6,10 @@ import { Container, H2 } from '@/styled/twc.styled'
 export const Contact = (): JSX.Element => {
   return (
     <footer id='contact' className='border-t border-neutral-750 mt-8'>
-      <Container className='flex flex-col sm:flex-row w-full py-8 lg:py-16 gap-4'>
+      <Container className='flex flex-row xs:flex-col sm:flex-row w-full py-8 lg:py-16 gap-4'>
         <H2 className='flex justify-center sm:justify-start items-center text-center w-full sm:w-4/12 lg:6/12'>Contáctame.</H2>
 
-        <div className='flex justify-evenly lg:justify-around items-start w-full sm:w-8/12 lg:w-6/12 gap-10'>
+        <div className='flex justify-end xs:justify-center sm:justify-evenly lg:justify-around items-start w-full sm:w-8/12 lg:w-6/12 xs:gap-10'>
           <SocialMediaItem
             className='justify-center'
             name='LinkedIn'
--- styled/twc.styled.ts
+++ styled/twc.styled.ts
@@ -3,10 +3,10 @@ import { twc } from 'react-twc'
 // Typography Components - Headers tags h1, h2, h3
 export const H1 = twc.h1`text-3xl md:text-7xl lg:text-8xl xl:text-9xl`
 export const H2 = twc.h2`text-2xl md:text-2xl lg:text-3xl xl:text-4xl`
-export const H3 = twc.h3`text-xl xs:text-2xl`
+export const H3 = twc.h3`text-xl`
 
 // Typography Components - Paragraph
-export const P = twc.p`text-lg xs:text-xl`
+export const P = twc.p`text-lg`
 
 // Typography Components - Span
 export const Span = twc.span`text-xl`
